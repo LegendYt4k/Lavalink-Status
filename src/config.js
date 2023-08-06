84697a851dd7e0b130c253e64ebe2d11b6ef9743
LegendYt4k <139982135+LegendYt4k@users.noreply.github.com>
2023-08-06 20:42:29 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  token: process.env.token || "", // Your bot token
-  channelId: process.env.channelId || "", //Channel Id you want to send the message
+  token: process.env.token || "MTEzNDQ0NjYwMjQwOTQ4NDMzOA.GYrXhk.eL0sjvCrXv1hdES_dd_A9KbzCbFo4Vg_OrJyq8", // Your bot token
+  channelId: process.env.channelId || "1137764661832458250", //Channel Id you want to send the message
 
   nodes: [
     {
-      host: "",
-      password: "",
+      host: "lava.lykhost.link",
+      password: "lavalykhost",
       port: 443,
-      retryDelay: 300000,
+      retryDelay: 10000,
       retryAmount: 25,
       identifier: "",
       secure: true,
